c650a789792991d7335bcf172319a417c16d6328
EmeraldBlock <yygengjunior@gmail.com>
2022-10-27 22:15:10 -0500

    support images and building just one mod

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -2,3 +2,32 @@ declare module "*.scss" {
     const css: string;
     export default css;
 }
+
+declare module "*.webp" {
+    const url: string;
+    export default url;
+}
+
+type Meta = {
+    website: string,
+    author: string,
+    name: string,
+    version: string,
+    id: string,
+    description: string,
+    minimumGameVersion: string,
+    doesNotAffectSavegame: boolean,
+
+    extra: {
+        authors: Array<{
+            name: string,
+            icon: string,
+        }>,
+        changelog: {
+            [version: string]: Array<string>,
+        },
+        source: string,
+        readme: string,
+        icon?: string,
+    },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -14,23 +14,7 @@ const BUILD = "./build/";
 const ENTRYS = ["index.ts", "index.js"];
 const META = "meta.js";
 
-process.chdir(path.dirname(url.fileURLToPath(import.meta.url)));
-
-const mods = (await fs.readdir(SRC, { withFileTypes: true })).filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
-
-const entry = Object.fromEntries(await Promise.all(mods.map(async (name) => {
-    const files = await fs.readdir(path.resolve(SRC, name));
-    const file = ENTRYS.find((val) => files.includes(val));
-    return [name, path.resolve(SRC, name, file)];
-})));
-
-const banners = Object.fromEntries(await Promise.all(mods.map(async (name) => {
-    const { id = "???", version = "???" } = (await import(url.pathToFileURL(path.resolve(SRC, name, META))).catch(() => undefined))?.default ?? {};
-    return [name, `${id} version ${version}
-source at https://github.com/EmeraldBlock/shapez.io-mods`];
-})));
-
-const fakeImports = {
+const FAKE_IMPORTS = {
     loader: "string-replace-loader",
     /** @type {import("string-replace-loader").Options} */
     options: {
@@ -39,59 +23,85 @@ const fakeImports = {
     },
 }
 
-/** @type {webpack.Configuration} */
-export default ({
-    entry,
-    output: {
-        path: path.resolve(process.cwd(), BUILD),
-        filename: "[name].js",
-        clean: {
-            dry: true,
-        },
-    },
-    mode: "production",
-    resolve: {
-        extensions: [".ts", "..."],
-        alias: {
-            "shapez.io": path.resolve(config.shapezDir, "src/css"),
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /.ts$/,
-                use: [
-                    fakeImports,
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true,
-                        },
-                    },
-                ],
+process.chdir(path.dirname(url.fileURLToPath(import.meta.url)));
+
+export default async (env) => {
+    const { mod } = env;
+    const mods = (mod === undefined)
+        ? (await fs.readdir(SRC, { withFileTypes: true })).filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name)
+        : [mod];
+    
+    const entry = Object.fromEntries(await Promise.all(mods.map(async (name) => {
+        const files = await fs.readdir(path.resolve(SRC, name));
+        const file = ENTRYS.find((val) => files.includes(val));
+        if (file === undefined) throw new Error(`Mod ${name} is missing an entry point!`);
+        return [name, path.resolve(SRC, name, file)];
+    })));
+    
+    const banners = Object.fromEntries(await Promise.all(mods.map(async (name) => {
+        const { id = "???", version = "???" } = (await import(url.pathToFileURL(path.resolve(SRC, name, META))).catch(() => undefined))?.default ?? {};
+        return [name, `${id} version ${version}
+    source at https://github.com/EmeraldBlock/shapez.io-mods`];
+    })));
+
+    /** @type {webpack.Configuration} */
+    return ({
+        entry,
+        output: {
+            path: path.resolve(process.cwd(), BUILD),
+            filename: "[name].js",
+            clean: {
+                dry: true,
             },
-            {
-                test: /.js$/,
-                use: fakeImports,
+        },
+        mode: "production",
+        resolve: {
+            extensions: [".ts", "..."],
+            alias: {
+                "shapez.io": path.resolve(config.shapezDir, "src/css"),
             },
-            {
-                test: /.scss$/,
-                use: {
-                    loader: "sass-loader",
-                    options: {
-                        sassOptions: {
-                            quietDeps: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /.ts$/,
+                    use: [
+                        FAKE_IMPORTS,
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                transpileOnly: true,
+                            },
                         },
-                    },
+                    ],
                 },
-                type: "asset/source",
-            },
+                {
+                    test: /.js$/,
+                    use: FAKE_IMPORTS,
+                },
+                {
+                    test: /.scss$/,
+                    use: {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                quietDeps: true,
+                            },
+                        },
+                    },
+                    type: "asset/source",
+                },
+                {
+                    test: /.webp$/,
+                    type: "asset/inline",
+                },
+            ],
+        },
+        optimization: {
+            minimizer: [new TerserPlugin({ extractComments: false })],
+        },
+        plugins: [
+            new webpack.BannerPlugin((data) => banners[data.chunk.name]),
         ],
-    },
-    optimization: {
-        minimizer: [new TerserPlugin({ extractComments: false })],
-    },
-    plugins: [
-        new webpack.BannerPlugin((data) => banners[data.chunk.name]),
-    ],
-});
+    });
+};
